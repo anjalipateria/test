2e77d4d25525c37efc55055db151ae3512bb3a4f
Manish Sanwle <manishsanwle@gmail.com>
2019-09-28 17:26:42 +0530

    Initial Commit with front page designed.
    Signed-off-by: Manish Sanwle <manishsanwle@gmail.com>

---
+++ application/views/index.php
@@ -0,0 +1,52 @@
+<div class="container">
+        <div class="row">
+            <div class="col-md-6 mt-5">
+                <h3>test helps you connect and share with the people in your life.</h3>
+                <img src="<?php echo base_url();?>assets/images/homepageImage.png" alt="Homepage Image" title="Homepage Image"/>
+            </div>
+            <div class="col-md-6 mt-5">
+                <h1>Create an account</h1>
+                <h3>It's quick and easy.</h3>
+                
+                <form>
+  <div class="form-row">
+    <div class="form-group col-md-6">
+      <label for="inputfirstname">First Name</label>
+      <input type="text" class="form-control" placeholder="First Name">
+    </div>
+    <div class="form-group col-md-6">
+      <label for="inputlastname">Last Name</label>
+      <input type="text" class="form-control" placeholder="Last Name">
+    </div>
+  </div>
+  <div class="form-group">
+      <label for="inputemail">Email</label>
+      <input type="email" class="form-control" placeholder="Email">
+  </div>
+  <div class="form-group">
+      <label for="inputpassword">Password</label>
+      <input type="password" class="form-control" placeholder="Password">
+  </div>
+  <div class="form-group">
+      <label for="inputconfpassword">Confirm Password</label>
+      <input type="password" class="form-control" placeholder="Confirm Password">
+  </div>
+  <div class="form-group">
+      <label for="inputgender">Gender</label>
+  </div>
+  <div class="form-group">
+      <div class="form-check form-check-inline">
+  <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="Female">
+  <label class="form-check-label" for="inlineRadio1">Female</label>
+</div>
+<div class="form-check form-check-inline">
+  <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="Male">
+  <label class="form-check-label" for="inlineRadio2">Male</label>
+</div>
+  </div>
+  <button type="submit" class="btn btn-warning">Register</button>
+</form>
+            </div>
+        </div>
+</div>
+    
---
+++ application/views/template/footer.php
@@ -0,0 +1,13 @@
+<footer>
+    <div class="col-md-12 text-light bg-danger text-center mt-5">
+        <p>&copy; Copyright test 2019. All rights reserved.</p>
+    </div>
+</footer>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="<?php echo base_url();?>assets/js/jquery-3.3.1.min.js"></script>
+    <script src="<?php echo base_url();?>assets/js/angular.min.js"></script>
+    <script src="<?php echo base_url();?>assets/js/bootstrap.min.js"></script>
+  </body>
+</html>
